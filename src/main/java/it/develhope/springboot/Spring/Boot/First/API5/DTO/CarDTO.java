be836005edfe2ca7a281316d51204cea5bc5e8c6
Alessio Limina <alessio.limina@gmail.com>
2023-04-17 13:36:51 +0200

    Exercise Spring Boot First API - 05 - fixing updated, needs to be tested

--- src/main/java/it/develhope/springboot/Spring/Boot/First/API5/DTO/CarDTO.java
+++ src/main/java/it/develhope/springboot/Spring/Boot/First/API5/DTO/CarDTO.java
@@ -2,19 +2,23 @@ package it.develhope.springboot.Spring.Boot.First.API5.DTO;
 
 import it.develhope.springboot.Spring.Boot.First.API5.entity.Car;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.ToString;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.modelmapper.ModelMapper;
 
 @Data
 @ToString
 public class CarDTO {
 
-    @Autowired
-    private Car car;
+    @NotBlank (message = "Mandatory")
+    private String id;
 
-    public CarDTO(Car car) {
-        this.car = car;
-    }
+    @NotBlank (message = "Mandatory")
+    private String modelName;
+
+    private double price;
+
+
+
 }
+
--- src/main/java/it/develhope/springboot/Spring/Boot/First/API5/controller/CarController.java
+++ src/main/java/it/develhope/springboot/Spring/Boot/First/API5/controller/CarController.java
@@ -3,6 +3,7 @@ package it.develhope.springboot.Spring.Boot.First.API5.controller;
 import it.develhope.springboot.Spring.Boot.First.API5.DTO.CarDTO;
 import it.develhope.springboot.Spring.Boot.First.API5.entity.Car;
 import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,8 +12,11 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api")
 public class CarController {
+
     @GetMapping("/")
-    public String getNewCar (@RequestParam Car car){
+    public String getNewCar (@RequestParam CarDTO carDTO){
+        ModelMapper modelMapper = new ModelMapper();
+        Car car = modelMapper.map(carDTO, Car.class);
         return car.toString();
     }
     @PostMapping("/")
@@ -20,5 +24,6 @@ public class CarController {
         return car.toString() + " " + HttpStatus.CREATED;
     }
 }
+
 
 //Documentazione Postman: https://documenter.getpostman.com/view/26121086/2s93XwyimR
