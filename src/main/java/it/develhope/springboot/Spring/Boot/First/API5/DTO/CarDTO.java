dc00db49350e866afb8ff065263e6ad3804a2125
Alessio Limina <alessio.limina@gmail.com>
2023-04-24 12:39:40 +0200

    Exercise Spring Boot First API - 05 - Fixed

--- src/main/java/it/develhope/springboot/Spring/Boot/First/API5/DTO/CarDTO.java
+++ src/main/java/it/develhope/springboot/Spring/Boot/First/API5/DTO/CarDTO.java
@@ -1,24 +1,22 @@
 package it.develhope.springboot.Spring.Boot.First.API5.DTO;
-
-import it.develhope.springboot.Spring.Boot.First.API5.entity.Car;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.modelmapper.ModelMapper;
 
 @Data
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class CarDTO {
 
     @NotBlank (message = "Mandatory")
     private String id;
-
     @NotBlank (message = "Mandatory")
     private String modelName;
-
     private double price;
-
-
 
 }
 
--- src/main/java/it/develhope/springboot/Spring/Boot/First/API5/controller/CarController.java
+++ src/main/java/it/develhope/springboot/Spring/Boot/First/API5/controller/CarController.java
@@ -1,29 +1,23 @@
 package it.develhope.springboot.Spring.Boot.First.API5.controller;
 
 import it.develhope.springboot.Spring.Boot.First.API5.DTO.CarDTO;
-import it.develhope.springboot.Spring.Boot.First.API5.entity.Car;
 import jakarta.validation.Valid;
-import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class CarController {
 
     @GetMapping("/")
-    public String getNewCar (@RequestParam CarDTO carDTO){
-        ModelMapper modelMapper = new ModelMapper();
-        Car car = modelMapper.map(carDTO, Car.class);
-        return car.toString();
+    public CarDTO getNewCar (){
+        return new CarDTO();
     }
     @PostMapping("/")
-    public String postNewCar (@Valid @RequestBody CarDTO car){
-        return car.toString() + " " + HttpStatus.CREATED;
+    public ResponseEntity<String> postNewCar (@Valid @RequestBody CarDTO car){
+        return ResponseEntity.status(HttpStatus.CREATED).body(car.toString());
     }
 }
 
 
-//Documentazione Postman: https://documenter.getpostman.com/view/26121086/2s93XwyimR
